0c104250a5a8216bd24e4efa12238a869584d7d9
Daniel <danielpascualbernal@gmail.com>
2026-02-11 19:24:25 +0100

    Fix: Prohibit negative numbers

--- calculator.java
+++ calculator.java
@@ -1,5 +1,31 @@
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
+    }    
+}
+
+public class Calculator {
+    public int add(int a, int b) {
+        System.out.println("Adding numbers");
+        return a + b;
     }
 }
+
+public class Calculator {
+    public int add(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException();
+        }
+        return a + b;
+    }
+}
+
+public class Calculator {
+    public int add(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Negative numbers not allowed");
+        }
+        return a + b;
+    }
+}
+
